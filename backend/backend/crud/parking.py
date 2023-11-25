bd88cabad5777ca0a5dd5273fd518ac82522f790
Ilin Ivan <buran05@icloud.com>
2023-11-26 00:50:56 +0300

    fix favorites routes + add peronal search for /parcking

--- backend/backend/crud/parking.py
+++ backend/backend/crud/parking.py
@@ -1,11 +1,18 @@
+from typing import Optional
+
 from sqlalchemy.orm import Session
 from backend import models, schemas, serializers
 
 
-def get_parking_list(db: Session, payload: schemas.GetParkingList) -> list[models.Parking]:
+def get_parking_list(db: Session, user_id: Optional[int] = None):
+    try:
+        db_user = db.query(models.User).filter(models.User.id == user_id).first()
+        favorite_parking_ids = [parking.id for parking in db_user.favorite_parkings]
+    except AttributeError:
+        favorite_parking_ids = []
     db_parking_list = db.query(models.Parking).all()
 
-    return db_parking_list
+    return db_parking_list, favorite_parking_ids
 
 
 def add_to_favorite(
